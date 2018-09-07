534f5e0c4f4d4c49a3d2f0582a823835d9d4e52a
bisousnours <agnesgaspard@hotmail.fr>
2018-09-07 16:42:37 +0200

    report added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,62 +6,24 @@ const app = new Vue({
 	data: {
 		pageActuelle: 'PageTop5',
 		contenuActuel: 'ResultatPositif',
-		listeRecettes: [
-			{titre: '1', type: 'entree', nouveau: 'o', description: 'a', avis:'1', ingredients: [
-			{nom: 'lait'},
-			{nom: 'oeufs'},
-			{nom: 'dinde'}
-			], instructions: [
-			{texte: 'Instru1'},
-			{texte: 'Instru2'},
-			{texte: 'Instru3'}
-			], auteur: 'administrateur', image: 'https://img-3.journaldesfemmes.fr/7SmUz3I3k2bvjPMKeV1BpOQungQ=/750x/smart/image-icu/10018962_1349466830.jpg'},
-
-			{titre: '2', type: 'dessert', nouveau: 'o', description: 'a', avis:'5', ingredients: [
-			{nom: 'eau'},
-			{nom: 'oeufs'},
-			{nom: 'jambon'}
-			], instructions: [
-			{texte: 'Instru1'},
-			{texte: 'Instru2'},
-			{texte: 'Instru3'}
-			], auteur: 'administrateur', image: 'https://img-3.journaldesfemmes.fr/7SmUz3I3k2bvjPMKeV1BpOQungQ=/750x/smart/image-icu/10018962_1349466830.jpg'},
-
-			{titre: '3', type: 'dessert', nouveau: 'n', description: 'a', avis:'4', ingredients: [
-			{nom: 'eau'},
-			{nom: 'oeufs'},
-			{nom: 'dinde'}
-			], instructions: [
-			{texte: 'Instru1'},
-			{texte: 'Instru2'},
-			{texte: 'Instru3'}
-			], auteur: 'administrateur', image: 'https://img-3.journaldesfemmes.fr/7SmUz3I3k2bvjPMKeV1BpOQungQ=/750x/smart/image-icu/10018962_1349466830.jpg'},
-
-			{titre: '4', type: 'plat', nouveau: 'n', description: 'a', avis:'3', ingredients: [
-			{nom: 'lait'},
-			{nom: 'oeufs'},
-			{nom: 'jambon'}
-			], instructions: [
-			{texte: 'Instru1'},
-			{texte: 'Instru2'},
-			{texte: 'Instru3'}
-			], auteur: 'administrateur', image: 'https://img-3.journaldesfemmes.fr/7SmUz3I3k2bvjPMKeV1BpOQungQ=/750x/smart/image-icu/10018962_1349466830.jpg'}
-		],
-		listeCourses: [
-			{nom: 'Lait'},
-			{nom: 'Oeufs'},
-			{nom: 'Jambon'}
-		],
-		listeFavoris: [
-			{titre: '1'},
-			{titre: '2'},
-			{titre: '3'}
-		],
+		listeRecettes: [{}],
 		filtreType: '',
 		filtreIngredient: '',
 		filtreAvis: '5',
 		filtreNouveau: '',
 		recetteAAfficher: [{titre: '', description: '', avis:'', ingredients: [], instructions: [], auteur: '', image: null}],
+    propositionTitre: '',
+    propositionType: '',
+    propositionDescription: 'Bon appétit !',
+    propositionImage: null,
+    propositionIngredient: '',
+    propositionListeIngredients: [],
+    propositionInstructions: '',
+    propositionNouveau:'',
+    propositionAvis:'',
+    propositionAuteur:'',
+    modificationIndex:'',
+    modif: false,
 		myList:[],
 		name: '',
     /*Pour page connexion*/
@@ -74,23 +36,30 @@ const app = new Vue({
 		confirmation_mdp:'true',
     password:'',
 		mot_de_passe: 'password',
-		mesClients:[],
-		listeClients: [
-        {nom_utilisateur: ''},
-        {mot_de_passe: ''},
-    ]
+		listeUtilisateurs: [{}],
+    favorisUtilisateur:[],
+    coursesUtilisateur:[]
 	},
 	created () {
 	    // Ici, l'utilisation d'une fonction flêchée () => {} plutôt que function () {} est primordial !
 	    // sans fonction fléchée, this.myList = ... ne fonctionnera pas comme prévu
-	    this.$http.get('/list')
-	      .then(list => {
-	        console.log('affichage de ma liste', list)
-	        this.myList = list.data
+	    this.$http.get('/bddRecettes')
+	      .then(bddRecettes => {
+	        console.log('affichage de ma liste', bddRecettes)
+	        this.listeRecettes = bddRecettes.data
 	      })
 	      .catch(err => {
 	        console.log('error', err)
 	      })
+        
+      this.$http.get('/bddUtilisateurs')
+	    .then(bddUtilisateurs => {
+	    	console.log('affichage de ma liste', bddUtilisateurs)
+	    	this.listeUtilisateurs = bddUtilisateurs.data
+	    })
+	    .catch(err => {
+	    	console.log('error', err)
+	    })
 	},
 	methods: {
     affichageFiltre (){
@@ -102,12 +71,12 @@ const app = new Vue({
 					recette.avis === this.filtreAvis)
 			} else if (this.filtreNouveau !== ''){
 				return this.listeRecettes.filter(recette => 
-					recette.nouveau === 'o')
+					recette.nouveau === 'oui')
 			} else if (this.filtreIngredient !== '') {
 				return this.listeRecettes.filter(recette => {
 					for(var i in Object.keys(recette.ingredients)){
 						if (recette.ingredients[i].nom===this.filtreIngredient){
-							return true
+              return true
 						}
 					}
 				})
@@ -119,27 +88,39 @@ const app = new Vue({
 				this.filtreType = filtreType
 				this.filtreAvis = filtreAvis
 				this.filtreNouveau = filtreNouveau
+        this.reinitialisationModif()
 				if (this.filtreType!=='' || this.filtreAvis!=='' || this.filtreNouveau!==''){
 					this.filtreIngredient = ''
 					this.contenuActuel = 'ResultatPositif'
 				}else if (this.filtreIngredient !== ''){
 					this.filtreIngredient = this.filtreIngredient.toLowerCase()
 					return this.listeRecettes.filter(recette => {
+            for(var i in Object.keys(recette.ingredients)){
+							if (recette.ingredients[i].nom===this.filtreIngredient){
+								trouve = 1
+                this.contenuActuel = 'ResultatPositif'
+								return true
+							}
+						}
 						if(trouve === 0){
 							this.contenuActuel = 'ResultatNegatif'
 						}else{
 							this.contenuActuel = 'ResultatPositif'
 						}
-						for(var i in Object.keys(recette.ingredients)){
-							if (recette.ingredients[i].nom===this.filtreIngredient){
-								trouve = 1
-								return true
-							}
-						}
 					})
 				}
-				
 		},
+    reinitialisationModif(){
+      if(this.modif===true){
+            this.modif=false
+          }
+      this.propositionTitre=''
+      this.propositionType=''
+      this.propositionDescription='Bon appétit !'
+      this.propositionListeIngredients=[]
+      this.propositionInstructions=''
+      this.propositionImage=''
+    },
     affichageRecette(page, recette){
 			this.pageActuelle = page
 			this.recetteAAfficher.titre = recette.titre
@@ -151,20 +132,103 @@ const app = new Vue({
 			this.recetteAAfficher.image = recette.image
 		},
 		affichageRecetteAleatoire(page){
-			this.pageActuelle = page
 			var tailleListeRecettes = 0
 			for(var i in this.listeRecettes){
 				tailleListeRecettes++
 			}
 			const index = Math.floor(Math.random() * tailleListeRecettes);
-			this.recetteAAfficher.titre = this.listeRecettes[index].titre
-			this.recetteAAfficher.description = this.listeRecettes[index].description
-			this.recetteAAfficher.avis = this.listeRecettes[index].avis
-			this.recetteAAfficher.ingredients = this.listeRecettes[index].ingredients
-			this.recetteAAfficher.instructions = this.listeRecettes[index].instructions
-			this.recetteAAfficher.auteur = this.listeRecettes[index].auteur
-			this.recetteAAfficher.image = this.listeRecettes[index].image
+      this.affichageRecette(page, this.listeRecettes[index])
 		},
+    ajouterNouvelIngredient(){
+      this.propositionListeIngredients.push({
+        nom: this.propositionIngredient
+      })
+      this.propositionIngredient = ''
+    },
+    ajouterNouvelleRecette(){
+      this.$http.post('/bddRecettes', {
+          titre: this.propositionTitre,
+          type: this.propositionType,
+          nouveau: 'oui',
+          description: this.propositionDescription, 
+          avis:'1', 
+          ingredients: this.propositionListeIngredients,
+          instructions: this.propositionInstructions, 
+          auteur: this.input_identifiant,
+          image: this.propositionImage 
+        
+        })
+        .then(() => {
+          this.listeRecettes.push({
+            titre: this.propositionTitre,
+            type: this.propositionType,
+            nouveau: 'oui', 
+            description: this.propositionDescription, 
+            avis:'1', 
+            ingredients: this.propositionListeIngredients,
+            instructions: this.propositionInstructions, 
+            auteur: this.input_identifiant,
+            image: this.propositionImage 
+          })
+        })
+      .then(() => {
+        this.propositionTitre=''
+        this.propositionType=''
+        this.propositionDescription='Bon appétit !'
+        this.propositionListeIngredients=[]
+        this.propositionInstructions=''
+        this.propositionImage=''
+        this.chgmtPageEtFiltre('PageTop5', '', '5', '')
+      }) 
+    },
+    supprimerRecette(index){
+      this.$http.delete('/bddRecettes/' + index,{	})
+        .then(() => {
+          this.listeRecettes.splice(index, 1)
+        })
+    },
+    affichageModificationRecette(index,recette){
+      this.modif=true
+      this.pageActuelle='PageProposition'
+      this.propositionTitre=recette.titre
+      this.propositionType=recette.type
+      this.propositionNouveau=recette.nouveau
+      this.propositionAvis=recette.avis
+      this.propositionAuteur=recette.auteur
+      this.propositionDescription=recette.description
+      this.propositionListeIngredients=recette.ingredients
+      this.propositionInstructions=recette.instructions
+      this.propositionImage=recette.image
+      this.modificationIndex=index
+    },
+    modifierRecette(){
+      
+     this.$http.put('/bddRecettes/' +this.modificationIndex,{	
+        titre: this.propositionTitre,
+        type: this.propositionType,
+        nouveau: this.propositionNouveau, 
+        description: this.propositionDescription, 
+        avis:this.propositionAvis, 
+        ingredients: this.propositionListeIngredients,
+        instructions: this.propositionInstructions, 
+        auteur: this.propositionAuteur, 
+        image: this.propositionImage 
+      })
+        .then(() => {
+          this.listeRecettes[this.modificationIndex].titre= this.propositionTitre,
+          this.listeRecettes[this.modificationIndex].type= this.propositionType,
+          this.listeRecettes[this.modificationIndex].nouveau= this.propositionNouveau, 
+          this.listeRecettes[this.modificationIndex].description= this.propositionDescription, 
+          this.listeRecettes[this.modificationIndex].avis= this.propositionAvis, 
+          this.listeRecettes[this.modificationIndex].ingredients= this.propositionListeIngredients,
+          this.listeRecettes[this.modificationIndex].instructions= this.propositionInstructions, 
+          this.listeRecettes[this.modificationIndex].auteur= this.propositionAuteur, 
+          this.listeRecettes[this.modificationIndex].image= this.propositionImage 
+        })
+          .then(() => {
+            this.chgmtPageEtFiltre('PageTop5', '', '5', '')
+          })
+    },
     sendNewElement () {
         this.$http.post('/list', {
           name: this.name
@@ -181,90 +245,124 @@ const app = new Vue({
           this.myList.splice(index, 1)
         })
     },
-    modifVisibilite: function(){
+    modifVisibilite(){
     this.mot_de_passe = this.mot_de_passe === 'password' ? 'text' : 'password';
     },
-		fonctionConnexion : function() {
-			/*Affectation des données du serveur*/
-			if(this.input_identifiant !== undefined){
-				this.listeClients.nom_utilisateur = this.input_identifiant;
-			}
-			else{
-				this.listeClients.nom_utilisateur = undefined
-			}
-			if(this.input_mdp !== undefined){
-				this.listeClients.mot_de_passe = this.input_mdp;
-			}
-			else{
-				this.listeClients.mot_de_passe = undefined
-			}
-			/*Envoi et connexion*/
-			if(this.listeClients.nom_utilisateur !== undefined && this.listeClients.mot_de_passe !== undefined){
-				this.est_connecte = true;
-				this.identifiant = true;
-				this.mdp = true;
-				this.$http.post('/client', {
-					listeClients: this.listeClients
-				})
-				.then(() => {
-					this.mesClients.push({
-						listeClients: this.listeClients
-					})
-				})
-				console.log('le client est : ' + this.listeClients.nom_utilisateur + ' mdp : ' + this.listeClients.mot_de_passe)
-				this.pageActuelle = 'PageTop5'
-			}
+		fonctionConnexion() {   
+		  	if(this.input_identifiant !== undefined && this.input_mdp !== undefined){
+		  		for(var user in this.listeUtilisateurs){
+		  			if(this.input_identifiant === this.listeUtilisateurs[user].nom_utilisateur){
+                this.identifiant = true;
+                if(this.input_mdp === this.listeUtilisateurs[user].mot_de_passe){
+                    this.mdp = true;
+                    this.est_connecte = true;
+                    this.favorisUtilisateur = this.listeUtilisateurs[user].favoris;
+                    this.coursesUtilisateur = this.listeUtilisateurs[user].liste_courses;
+                    this.chgmtPageEtFiltre('PageTop5', '', '5', '')
+                    return true
+                }
+                else{
+                  this.mdp = false;
+                  console.log('mdp non valide')
+                }
+		  			}
+		  			else {
+		  				this.identifiant = false;
+		  			}
+		  		}
+		  	}
 			/*Pas de connexion*/
-			else{
+			else if (this.input_identifiant === undefined){
 				this.est_connecte = false;
+				this.identifiant = false;
 			}
-		},
-		fonctionInscription : function() {
+			else if (this.input_mdp === undefined){
+				this.est_connecte = false;
+				this.mdp = false;
+			}
+		
+    },
+		fonctionInscription(){
+      if(this.input_identifiant !== undefined && this.input_mdp !== undefined){
+		  		console.log('nom et mdp = entrés')
+		  		for(var user in this.listeUtilisateurs){
+            console.log(this.listeUtilisateurs[user].nom_utilisateur)
+		  			if(this.input_identifiant === this.listeUtilisateurs[user].nom_utilisateur){
+                this.identifiant = false;
+                console.log(this.identifiant + 'existe déjà')
+                this.est_connecte = false;
+		  			}
+		  			else {
+              this.listeUtilisateurs.push({
+                nom_utilisateur: this.input_identifiant,
+                mot_de_passe: this.input_mdp
+              })
+              this.input_identifiant = '';
+              this.input_mdp = '';
+              this.identifiant = true;
+              this.mdp = true;
+              this.est_connecte = true;
+		  			}
+		  		}
+		  	}
+			/*Pas de connexion*/
+			else if (this.input_identifiant === undefined){
+				this.est_connecte = false; 
+				this.identifiant = false;
+				console.log('rien d\'inscrit')
+			}
+			else if (this.input_mdp === undefined){
+				this.est_connecte = false;
+				this.mdp = false;
+				console.log('rien d\'inscrit')
+			}
+		
+
+      
 			/*Affectation des données du serveur*/
-			if(this.input_identifiant !== undefined){
-				this.listeClients.nom_utilisateur = this.input_identifiant;
+			/*if(this.input_identifiant !== undefined){
+				this.listeUtilisateurs.nom_utilisateur = this.input_identifiant;
 			}
 			else{
-				this.listeClients.nom_utilisateur = undefined
+				this.listeUtilisateurs.nom_utilisateur = undefined
 			}
 			
 			if(this.input_mdp !== undefined && this.input_confirmation_mdp === this.input_mdp){
-				this.listeClients.mot_de_passe = this.input_mdp;
+				this.listeUtilisateurs.mot_de_passe = this.input_mdp;
 			}
 			else{
-				this.listeClients.mot_de_passe = undefined
+				this.listeUtilisateurs.mot_de_passe = undefined
 			}
-			/*Envoi et connexion */
-			if(this.listeClients.nom_utilisateur !== undefined && this.listeClients.mot_de_passe !== undefined){
+			
+			if(this.listeUtilisateurs.nom_utilisateur !== undefined && this.listeClients.mot_de_passe !== undefined){
 				this.est_connecte = true;
 				this.identifiant = true;
 				this.mdp = true;
 				this.$http.post('/client', {
-					listeClients: this.listeClients
-				})
+					listeUtilisateurs: this.listeUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateursvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+				})listeUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateursvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 				.then(() => {
 					this.mesClients.push({
-						listeClients: this.listeClients
+						listeUtilisateurs: this.listeUtilisateurs
 					})
 				})
-				console.log('le client est : ' + this.listeClients.nom_utilisateur + ' mdp : ' + this.listeClients.mot_de_passe)
-				this.pageActuelle = 'PageTop5'
+				console.log('le client est : ' + this.listeUtilisateursisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateurslisteUtilisateursliste.nom_utilisateur + ' mdp : ' + this.listeClients.mot_de_passe)
+				this.chgmtPageEtFiltre('PageTop5', '', '5', '')
 			}
-			/*Pas de connexion*/
+			Pas de connexion
 			else{
 				this.est_connecte = false;
-			}
+			}*/
 		},
-		fonctionDeconnection : function() {
-			if(this.est_connecte == true){
+		fonctionDeconnection() {
+      	if(this.est_connecte == true){
 				this.est_connecte = false;
-				this.listeClients.nom_utilisateur = undefined;
-				this.listeClients.mot_de_passe = undefined;
 				this.input_identifiant = undefined;
 				this.input_mdp = undefined;
 				this.input_confirmation_mdp = undefined;
-				this.pageActuelle = 'PageDeconnection'
+				this.chgmtPageEtFiltre('PageTop5', '', '5', '')
 			}
+
 		}
 	}
 })
--- routes/index.js
+++ routes/index.js
@@ -20,34 +20,134 @@ router.post('/list', (req, res) => {
 })
 
 router.delete('/list/:id', (req, res) => {
-	console.log('coté serveur : ' + req.params.id)
-	/*var index=list.findIndex(index => list.index === req.params.id)*/
-
+  var index = -1
+  for(var i in list){
+    if (i===req.params.id) {
+      index = i
+    }
+  }
+  list.splice(index, 1)
+  res.send('OK')
 })
 
 /* BDD des identifiants des clients*/
-const client = [
-  { nom_utilisateur: 'admin' , mot_de_passe: 'admin'}, 
-  { nom_utilisateur: 'user1' , mot_de_passe: 'user1'}
+const bddUtilisateurs = [
+  { nom_utilisateur: 'admin' , mot_de_passe: 'admin', liste_courses: [{nom: 'Lait'},{nom: 'Oeufs'},{nom: 'Jambon'}], favoris: [{titre: 'Moussaka'},{titre: 'Pâte à crêpe'}]}, 
+  { nom_utilisateur: 'user1' , mot_de_passe: 'user1', liste_courses: [{nom: 'tomates'},{nom: 'herbes de provence'},{nom: 'moutarde'}], favoris: [{titre: 'Tarte à la tomate'},{titre: 'Flan au carottes'}]}
 ]
 
-router.post('/client', (req, res) => {
-  res.json(client)
+router.get('/bddUtilisateurs', (req, res) => {
+  res.json(bddUtilisateurs)
 })
 
-router.post('/client', (req, res) => {
-  list.push({
+router.post('/bddUtilisateurs', (req, res) => {
+  bddUtilisateurs.push({
     nom_utilisateur: req.body.nom_utilisateur,
     mot_de_passe: req.body.mot_de_passe
   })
   res.send('OK')
 })
 
-/* GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Trouve ta recette !' });
-});
-*/
+
+const bddRecettes = [
+      {titre: 'Moussaka', type: 'Plat', nouveau: 'non', description: 'La vrai moussaka grecque !', avis:'1', ingredients: [
+      {nom: 'aubergines'},
+      {nom: 'pommes de terre'},
+      {nom: 'tomates'},
+      {nom: 'oignons'},
+      {nom: 'boeuf haché'},
+      {nom: 'huile'},
+      {nom: 'beurre'}
+      ], instructions: 'Préparation de la sauce tomate.\nEmincer les oignons et les faire blondir dans une petite casserole.\nAjouter les tomates coupées en gros dés et 2 bonnes cuillères à soupe d huile d olive, la cannelle, le miel, saler et poivrer. Laisser réduire le tout 25 minutes (ou plus, jusqu à arriver à la préparation de la viande) à feu moyen.\nOn doit obtenir une sauce, que l on peut homogénéiser avec du coulis de tomate tout prêt.\nPréparation des aubergines.\nDécouper les aubergines en rondelles (sans les peler), saler généreusement, et les laisser dégorger le temps de l étape suivante.\nConseil pour faire dégorger les aubergines : dans un plat, alterner une couche de tranches d aubergine salées et une couche de papier absorbant ou les laisser dans une passoire.\nPréparation des pommes de terre. \nEplucher et découper les pommes de terres en fines rondelles.\nDisposer les rondelles de pommes de terre au fond d un plat à gratin assez haut, huilé.\nArroser les pommes de terres d un peu (3 cuillères à soupe) du jus rendu par les tomates qui mijotent.\nPasser le plat de pommes de terre sous le grill 5 à 15 min selon votre four, pour qu elles dorent.\nMaintenant qu elles ont dégorgé, passer les rondelles d aubergine à la poêle à feu fort pour les faire griller un peu de chaque côté, réserver.\nPréparation de la viande. \nDans un faitout, faire revenir la viande hachée au beurre à feu assez fort, saler, poivrer et retirer l eau rendue par la viande.\nAjouter la sauce tomate-oignons et baisser le feu (très doux).\nSortir le plat de pommes de terres du four et le préchauffer le four à 200°C (thermostat 6-7).\nPréparation de la béchamel. \nDans une petite casserole sur feu doux, faire blondir les 20 g de beurre, ajouter la farine et mélanger pour obtenir un appareil homogène.\nIncorporer petit à petit le lait en n arrêtant jamais de remuer, ça peut prendre 10 bonnes minutes, on doit obtenir une sauce assez épaisse.\nSaler, poivrer et râper un peu de noix de muscade (c est fort, il faut en mettre peu).\nMontage de la moussaka.\nPar dessus la couche de pommes de terre, étaler la moitié de la viande hachée avec la tomate, puis la moitié des aubergines, puis l autre moitié de viande, puis l autre moitié des aubergines, un filet d huile d olive et terminer par la béchamel.\nEnfourner le tout dans le four à 200°C (thermostat 6-7) et laisser cuire 1h (la béchamel doit croustiller et être dorée).'
+     , auteur: 'administrateur', 
+     image: 'https://image.afcdn.com/recipe/20180503/79002_w420h344c1cx1944cy1296.jpg'},
+
+      {titre: 'Flan au carottes', type: 'Entrée', nouveau: 'oui', description: 'Très facile à faire', avis:'5', ingredients: [
+      {nom: 'carottes'},
+      {nom: 'oeufs'},
+      {nom: 'beurre'},
+      {nom: 'crème'},
+      {nom: 'herbes de provence'}
+      ], instructions: 'Faire revenir les carottes coupées en lamelles dans un peu de beurre, puis les recouvrir de bouillon cube.\nBien laisser évaporer.\nMixer les carottes et ajouter tout le reste.\nVerser dans un moule beurré et cuire au bain-marie à four moyen pendant 35 minutes (vérifier la cuisson avec la pointe dun couteau).'
+       , auteur: 'administrateur', 
+       image: 'https://image.afcdn.com/recipe/20130328/24942_w420h344c1cx1000cy1342.jpg'},
+
+      {titre: 'Tarte à la tomate', type: 'Plat', nouveau: 'non', description: 'Plat de dernière minute', avis:'4', ingredients: [
+      {nom: 'pate brisée'},
+      {nom: 'tomates'},
+      {nom: 'herbes de provence'},
+      {nom: 'moutarde'},
+      {nom: 'huile'}
+      ], instructions: 'Précuire la pâte (percée avec une fourchette) 5 min à 450 degrés farenheit (230°C).\nPendant ce temps, couper les tomates en rondelles de 3 mm d épaisseur.\nSortir la pâte précuite du four. badigeonner le fond de moutarde de Dijon.\nDisposer les tranches de tomates. faire plusieurs épaisseurs.\nSaupoudrer d herbes de Provence + 2 pincées de sel.\nEnfourner pour 20 min à 450 farenheit puis couvrir avec papier d aluminium pour 20 min supplémentaires. \nAprès ces 20 min, enlever le papier aluminium et laisser cuire encore 5 min porte du four ouverte. \nDélicieux avec une salade verte.'
+       , auteur: 'administrateur', 
+       image: 'https://image.afcdn.com/recipe/20180208/77440_w420h344c1cx1824cy2736cxt0cyt0cxb3648cyb5472.jpg'},
+
+      {titre: 'Pâte à crêpe', type: 'Dessert', nouveau: 'non', description: 'Délicieux !', avis:'3', ingredients: [
+      {nom: 'farine'},
+      {nom: 'oeufs'},
+      {nom: 'beurre'},
+      {nom: 'sucre'},
+      {nom: 'sucre vanillé'},
+      {nom: 'lait'},
+      {nom: 'huile'}
+      ], instructions: 'Mélanger le tout pendant 1 minute puis tamiser avant de cuire.'
+       , auteur: 'administrateur', 
+       image: 'https://image.afcdn.com/recipe/20171019/73354_w420h344c1cx2808cy1872cxt0cyt0cxb5616cyb3744.jpg'}
+    
+]
+
+router.get('/bddRecettes', (req, res) => {
+  res.json(bddRecettes)
+})
+
+router.post('/bddRecettes', (req, res) => {
+  bddRecettes.push({
+    titre: req.body.titre,
+    type: req.body.type,
+    nouveau: 'o', 
+    description: req.body.description, 
+    avis:'1', 
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions, 
+    auteur: req.body.auteur,
+    image: req.body.image 
+  })
+  res.send('OK')
+})
+
+router.delete('/bddRecettes/:id', (req, res) => {
+  var index = -1
+  for(var i in bddRecettes){
+    if (i===req.params.id) {
+      index = i
+    }
+  }
+  if (index!== -1)  {
+    bddRecettes.splice(index, 1)
+  }
+  res.send('OK')
+})
+
+router.put('/bddRecettes/:id', (req, res) => {
+  var index = -1
+  for(var i in bddRecettes){
+    if (i===req.params.id) {
+      index = i
+    }
+  }
+  if (index !== -1){
+    bddRecettes[index].titre= req.body.titre,
+    bddRecettes[index].type= req.body.type,
+    bddRecettes[index].nouveau= req.body.nouveau, 
+    bddRecettes[index].description= req.body.description, 
+    bddRecettes[index].avis= req.body.avis, 
+    bddRecettes[index].ingredients= req.body.ingredients,
+    bddRecettes[index].instructions= req.body.instructions, 
+    bddRecettes[index].auteur= req.body.auteur,
+    bddRecettes[index].image= req.body.image 
+  }
+  res.send('OK')
+})
 
 /*A garder !! */
 module.exports = router;
